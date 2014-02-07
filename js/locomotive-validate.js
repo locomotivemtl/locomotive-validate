d840da8d9c893d9503a3650000b0bb84a1c80ac5
Stephen <stevo_junk@hotmail.com>
2014-02-07 09:35:11 -0500

    Ajouter validation pour les inputs, ajouter du regex dans un data

--- js/locomotive-validate.js
+++ js/locomotive-validate.js
@@ -1,5 +1,4 @@
 (function ( $ ) {    
-
 	/**
 	*	lvalidate Plugin
 	*	Applys to a <form> element.
@@ -13,12 +12,12 @@
 		/**
 		*	Default settings
 		*/
-        var defauts=
+        var defaults =
         {   
-            // "uniform" : false, // You decide the error handlings in the instanciation of the plugin.
-            // "popup"   : false, // You decide the error handlings in the instanciation of the plugin.
 			"requiredClass":"required",
 			"similarClass":"similar",
+			"requiredGroupClass":"required-group",
+			"regExpClass":"regexp",
 			"onError": function(e,c) {
 			
 			},
@@ -29,13 +28,28 @@
 			},
 			"custom_validation" : function(lvalidate) {
 				return true;
+			},
+			"invalid":function(o) {
+
 			}
         };
 		
+		if (typeof options == 'string' && $(this).data('lvalidate')) {
+			var obj = $(this).data('lvalidate');
+			switch (options) {
+				case 'validate':
+					obj.validate();
+					return this;
+				break;
+				default:
+					return this;
+				break;
+			}
+		}
 		/**
 		*	Extended settings
 		*/
-        var settings=$.extend(defauts, options); 
+        var settings=$.extend(defaults, options); 
 
 		$(this).each(function(i,e) {
 			// We want the actual form to be in the settings as "obj"
@@ -52,7 +66,7 @@
 		*	On submit, we wanna prevent default (the actual submit.)
 		*	Validation should occur there
 		*/
-		$(this).find('input[type=submit]').click(function(e) {	
+		$(this).on('click','input[type=submit]',function(e) {
 			e.preventDefault();
 			var form = $(this).parents('form');
 			var lvalidate = form.data('lvalidate');
@@ -61,6 +75,7 @@
 			// All the handlers for success or error are within the "lvalidate" datas.
 			lvalidate.validate();
 		});
+		return this;
 
     };
  
@@ -74,6 +89,7 @@
 *		{
 *			obj : [Form element],
 *			requiredClass : 'required',
+*			requiredGroupClass : "required-group"
 *			onError : function(e) {},
 *			onSuccess: function(e) {}
 *		}
@@ -96,6 +112,8 @@
 *	@todo Customize callbacks and rules
 *	@version 2013-08-15
 */
+
+
 var Locomotive_Validate = function(opts){
 
 	/**
@@ -110,14 +128,20 @@ var Locomotive_Validate = function(opts){
 	// Will contain invalid inputs.
 	this.invalidInputs = Array();
 	
-	this.validate = function() {
+	this.validate = function(opts) {
+		if (!opts) {
+
+		}
 		// Fits the old API
 		this.form = this.obj;
 		
 		// Required fields comes from the requiredClass setting
 		var required_fields = this.form.find('.'+this.settings.requiredClass);
 		var similar_fields = this.form.find('.'+this.settings.similarClass);
-		
+		var required_group_fields = this.form.find('.'+this.settings.requiredGroupClass);
+		var regex_fields = this.form.find('.'+this.settings.regExpClass);
+
+		this.form.find('.error').removeClass('error');
 		// Scoping
 		var that = this;
 		
@@ -136,36 +160,52 @@ var Locomotive_Validate = function(opts){
 				that.settings.onFirstError($(this),"First Error");
 			}
 		});
-		// Check required_fields FIRST
-		// if (!error) {
-			// Looping the inputs.
-			similar_fields.each(function(i,e) {
-				var tmp = that.validate_input($(this),'similar');
-				no_error = no_error && !tmp;
 
+		similar_fields.each(function(i,e) {
+			var tmp = that.validate_input($(this),'similar');
+			no_error = no_error && !tmp;
+
+			if (!no_error && first_input) {
+				first_input = false;
+				that.settings.onFirstError($(this),"First Error");
+			}
+		});
+
+		var group_fields_success;
+		required_group_fields.each(function(i,e) {
+			var _this = $(this);
+			var datas = _this.data();
+			var tmp = that.validate_input($(this),'required-group',datas);
+			no_error = no_error && !tmp;
 				if (!no_error && first_input) {
 					first_input = false;
 					that.settings.onFirstError($(this),"First Error");
 				}
-			});
-		// }
+		});
 
-
-		// if(no_error) {
-			// Last logic -> the user appended logic.
-			// Only if there are no errors yet.
-			if (typeof this.settings.custom_validation == 'function') {
-				var tmp = this.settings.custom_validation(this);
-				// @todo CHANGE LOGIC
-				// In this one we need a "TRUE" response instead of FALSE
-				no_error = no_error && tmp;
+		regex_fields.each(function(i,e) {
+			// var tmp = that.validate_input($(this));
+			var _this = $(this);
+			var datas = _this.data();
+			var tmp = that.validate_input($(this),'regex',datas);
+			no_error = no_error && !tmp;
 				if (!no_error && first_input) {
 					first_input = false;
-					that.settings.onFirstError(false,"Custom Error");
+					that.settings.onFirstError($(this),"First Error");
 				}
-			}
+		});
 
-		// }
+		if (typeof this.settings.custom_validation == 'function') {
+			var tmp = this.settings.custom_validation(this);
+			// @todo CHANGE LOGIC
+			// In this one we need a "TRUE" response instead of FALSE
+			no_error = no_error && tmp;
+			if (!no_error && first_input) {
+				first_input = false;
+				that.settings.onFirstError(false,"Custom Error");
+			}
+		}
+
 		
 		// If there's an error, heres the callback
 		// @todo create an Error object.
@@ -176,25 +216,38 @@ var Locomotive_Validate = function(opts){
 		}
 	};
 
-	this.validate_input = function(input, option) {
-		if (!option) {
-			option = "required";
+	this.validate_input = function(input, rule, options) {
+		if (!rule) {
+			rule = "required";
 		};
 		var $this = input;
 		// Scoping
 		var that = this;
+		var datas = $this.data();
+
+		if (typeof datas.regexp != 'undefined') {
+			rule = "regexp";
+		}
+
+		if (rule == "regexp") {
+			if (!this.matchRegExp($this,datas)) {
+				return that.error($this,'regex_err');
+			}
+			return that.success($this,'ok');
+		}
 		
-		if (option == "required") {
+		if (rule == "required") {
 			// Tagname, type and other necessary stuff
 			var tagName = $this.prop("tagName");
 			var type = $this.attr('type');
 			var name = $this.attr('name');
-			// console.log(name);
+
+
 			
 			//are we talking about inputs because it might be a select
 			if (tagName == "INPUT") {
 			   //is it a text input? 
-			   if (type == 'text' || type == 'password') { 
+			   if (type == 'text' || type == 'password' || type == 'phone' || type == 'tel') { 
 					if (this.isEmpty($this)) {
 						return that.error($this,'empty');
 					}
@@ -210,6 +263,13 @@ var Locomotive_Validate = function(opts){
 					}
 					return that.success($allradios,'ok');
 				}
+				//is it a radio input? 
+				if (type == 'checkbox') { 
+					if (!this.isChecked($this)) {
+						return that.error($this,'empty');
+					}
+					return that.success($this,'ok');
+				}
 
 				//validating emails
 				if (type == 'email') { 				
@@ -218,9 +278,7 @@ var Locomotive_Validate = function(opts){
 					}
 					return that.success($this,'ok');
 				}
-
 			}
-
 
 			//validating textarea
 			if (tagName == "TEXTAREA") {
@@ -239,30 +297,89 @@ var Locomotive_Validate = function(opts){
 				return that.success($this,'ok');                 
 			}
 		}
-		if (option == "similar") {
+
+		if (rule == "similar") {
 			if ($this.attr("data-related")) {
 				var related = $this.attr("data-related");
 				var related_inputs = this.form.find("[name="+related+"]");
-
 				if (this.isDifferent([$this,related_inputs])) {
 					return that.error(related_inputs,'similar',$this);
 				}
 				return that.success(related_inputs,'Similar');
-
 			};
 			return that.success($this,"no-related-elements");
 		};
+
+		if (rule == "required-group" && options) {
+			var isMultiple = false;
+			var fields;
+			if (options.required.indexOf('[]') > 0) {
+				isMultiple = true;
+				fields = options.required.substr(0,options.required.indexOf('[]'));
+			} else {
+				fields = options.required.replace(' ','').split(',');
+			}
+
+			var atLeastOne = false;
+			var fields_array = Array();
+
+			var elems = isMultiple ? that.form.find('[name^='+fields+']') : '';
+
+			if (elems.length) {
+
+				elems.each(function(i,e) {
+					var el = $(this);
+					if (el.length) {
+						fields_array.push(el);
+					}
+					// stop the loop if you got what you need.
+					if (!atLeastOne) {
+						if (el.length && !atLeastOne) {
+							var tmp = !that.isEmptySmart(el);
+							atLeastOne = !!tmp;
+						}
+					}
+				});
+
+			} else {
+
+				for (var i = 0; i < fields.length; i++) {
+					var el = that.form.find('[name='+fields[i]+']');
+					if (el.length) {
+						fields_array.push(el);
+					}
+					// stop the loop if you got what you need.
+					if (atLeastOne) {
+						continue;
+					}
+					if (el.length && !atLeastOne) {
+						var tmp = !that.isEmptySmart(el);
+						atLeastOne = !!tmp;
+					}
+				}
+
+			}
+
+			if (!atLeastOne) {
+				// return error on THIS being the container DIV with the DATAS
+				//  rule required_group
+				// Concerned fields.
+				return that.error($this,'required_group',fields_array);
+			}
+			return that.success($this,'required_group',fields_array);
+
+		}
 	}
 	
-	this.error = function(elem,code) {
+	this.error = function(elem,code,related_elems) {
 		// Error callback
-		this.settings.onError(elem,code);
+		this.settings.onError(elem,code,related_elems);
 		return true;
 	};
 
-	this.success = function(elem,code) {
+	this.success = function(elem,code,related_elems) {
 		// Success callback
-		this.settings.onSuccess(elem,code);
+		this.settings.onSuccess(elem,code,related_elems);
 		return false;
 	};
 	
@@ -282,7 +399,43 @@ var Locomotive_Validate = function(opts){
 	*	In any other case, you should use the jQuery instanciation
 	*
 	*/
-	
+	this.isEmptySmart = function(elem) {
+		var $this = elem;
+		var that = this;
+
+		var tagName = $this.prop("tagName");
+		var type = $this.attr('type');
+		var name = $this.attr('name');
+
+		if (tagName == "INPUT") {
+		   if (type == 'text' || type == 'password') { 
+				return this.isEmpty($this)
+			}
+
+			if (type == 'radio') { 
+				var $allradios =  this.obj.find('input[name=' + name + ']');
+				return this.isRadioEmpty($allradios);
+			}
+
+			if (type == 'checkbox') { 
+				return !this.isChecked($this);
+			}
+
+			//validating emails
+			if (type == 'email') { 				
+				return !this.isValidEmail($this);
+			}
+		}
+
+		//validating textarea
+		if (tagName == "TEXTAREA") {
+			return this.isEmpty($this);
+		}
+		if (tagName == "SELECT") {
+			return this.isSelectEmpty($this);            
+		}
+	}
+
 	// Check if val == '';
 	this.isEmpty = function(elem) {
 		return (elem.val() == '' || !elem.val() || elem.val() == '-1');
@@ -301,6 +454,9 @@ var Locomotive_Validate = function(opts){
 		return (this.isEmpty(elem.find("option:selected")));
 	}
 	
+	this.isChecked = function(elem) {
+		return elem.is(':checked');
+	}
 	// Check if all radio buttons are unselected
 	this.isRadioEmpty = function(elem) {
 						
@@ -317,6 +473,22 @@ var Locomotive_Validate = function(opts){
 		
 		return radioError;
 	}
+
+	/**
+	*	Check regEx rules
+	*/
+	this.matchRegExp = function(elem, data) {
+		var datas = data;
+		var testedValue = this.get_value(elem);
+		var modifiers = 'gi';
+		if (typeof datas['modifiers'] != 'undefined') {
+			modifiers = datas['modifiers'];
+		}
+		var regex = new RegExp(data.regexp, modifiers);
+
+		return regex.test(testedValue);
+	}
+
 
 	/** Can only be an array
 	* @param [array] aElem 
@@ -360,8 +532,11 @@ var Locomotive_Validate = function(opts){
 		//are we talking about inputs because it might be a select
 		if (tagName == "INPUT") {
 		   //is it a text input? 
-			if (type == 'text' || type == 'email' || type == 'password') { 
+			if (type == 'text' || type == 'email' || type == 'password' || type == 'tel') { 
 				return $this.val();
+		 	}
+		 	if (type == 'checkbox') {
+		 		return $this.is(':checked') ? $this.val() : '';
 		 	}
 			//is it a radio input? 
 			if (type == 'radio') { 
